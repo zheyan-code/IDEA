ca6dcf1e4b37b094c11f78f26ea71e9ca1b620b7
zheyan-code <xingkaiyang1991@gmail.com>
2025-01-04 15:58:34 +0800

    little change

---
+++ Starter/PalindromeIgnoreNonAlphanumeric.java
@@ -0,0 +1,32 @@
+package Starter;
+import java.util.Scanner;
+
+
+public class PalindromeIgnoreNonAlphanumeric {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s = sc.nextLine();
+        System.out.println(isPalindrome(s));
+
+    }
+    public static boolean isPalindrome(String s) {
+        String s1=filter(s);
+        String s2=reverse(s1);
+        return s1.equals(s2);
+    }
+    public static String filter(String s) {
+        StringBuilder sb=new StringBuilder();
+        for (int i=0;i<s.length();i++){
+            if(Character.isLetterOrDigit(s.charAt(i))){
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+    public static String reverse(String s) {
+        StringBuilder sb=new StringBuilder(s);
+        sb.reverse();
+        return sb.toString();
+    }
+}
---
+++ Starter/Test.java
@@ -0,0 +1,12 @@
+package Starter;
+
+public class Test {
+    private String text;
+    public Test(String s){
+        this.text=s;
+    }
+    public static void main(String[] args){
+        Test test=new Test("ABC");
+        System.out.println(test.text.toLowerCase());
+    }
+}
